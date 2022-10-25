fee0d624f7f5afb0ccdeca708fcb30544ad56994
Safwan <safwanridwan321@gmail.com>
2022-10-25 23:22:17 +0600

    added toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { AuthContext } from './AuthProvider/AuthProvider';
 import { router } from './routes/Routes';
 
 function App() {
+  const {toggle} = useContext(AuthContext);
+
   return (
-    <div>
+    <div data-theme={toggle ? "lofi" : "luxury"}>
       <RouterProvider router={router}></RouterProvider>
       <Toaster></Toaster>
     </div>
--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -13,6 +13,7 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(true);
+    const [toggle, setToggle] = useState(false);
 
     // functions
     // email password
@@ -62,7 +63,7 @@ const AuthProvider = ({ children }) => {
         }
     }, [])
 
-    const authInfo = { user, createUser, logIn, logOut, updateUser, googleLogIn, gitHubLogIn, loading };
+    const authInfo = { user, createUser, logIn, logOut, updateUser, googleLogIn, gitHubLogIn, loading, toggle, setToggle };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
