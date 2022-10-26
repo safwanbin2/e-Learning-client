387863d1c3f51e71bc415907b2babfc9320fbb03
Safwan <safwanridwan321@gmail.com>
2022-10-26 14:16:21 +0600

    simple correction

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -70,7 +70,7 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-3xl font-bold">Please, Login to account now!</h1>
+                    <h1 className="md:text-3xl text-xl font-bold">Please, Login to account now!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
                     <form onSubmit={handleSubmit} className="card-body">
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -46,9 +46,9 @@ const Register = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-3xl font-bold">Please, Create account now!</h1>
+                    <h1 className="md:text-3xl text-xl font-bold">Please, Create account now!</h1>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
                     <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
